2a2fa48ef9a3c38118a0e9e7b2d61d1f182f622e
Roboticol <kaushikguha68@gmail.com>
2022-01-28 22:49:28 +0530

    Init

---
+++ api/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import os
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model(
+    "D:\machine learning projects\\agriculture\saved_models\modelv4. better, faster, stronger")
+
+CLASS_NAMES = ['Potato___Early_blight',
+               'Potato___Late_blight',
+               'Potato___healthy',
+               'Tomato_Early_blight',
+               'Tomato_Late_blight',
+               'Tomato_healthy']
+
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
