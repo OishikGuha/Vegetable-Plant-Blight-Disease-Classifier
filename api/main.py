5553a5c62f3be62162eafdde27d5fa00e1a76457
OishikGuha <kaushikguha68@gmail.com>
2022-01-31 00:52:21 +0530

    Added an Image resizer because the AI's resizer is bugged.

--- api/main.py
+++ api/main.py
@@ -49,13 +49,14 @@ async def predict(
     file: UploadFile = File(...)
     # file: FileData = File(...)
 ):
-    image = read_file_as_image(await file.read())
+    image = tf.image.resize(read_file_as_image(await file.read()), (256, 256))
     img_batch = np.expand_dims(image, 0)
 
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
+
     return {
         'class': predicted_class,
         'confidence': float(confidence)
